e2233b49e74b7f8ad88c27b53d4b6c3d06f4b1d1
ajaythoram <115677014+ajaythoram@users.noreply.github.com>
2023-05-21 14:07:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const btn = document.getElementById("enterBtn");
 const para = document.getElementById("status")
 btn.addEventListener('click',()=>{
   const newheading = document.createElement('h1');
+	newHeading.id = para.id;
   newheading.textContent = para.textContent;
   para.parentNode.replaceChild(newheading,para);
 	 
