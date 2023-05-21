d2172ef28c6a88780e1b4ef7d4327db24271164e
ajaythoram <115677014+ajaythoram@users.noreply.github.com>
2023-05-21 14:02:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const btn = document.getElementById("enterBtn");
+const para = document.getElementById("status")
+btn.addEventListener('click',()=>{
+  const newheading = document.createElement('h1');
+  newheading.textContent = para.textContent;
+  para.parentNode.replaceChild(newheading,para);
+	 
+})
